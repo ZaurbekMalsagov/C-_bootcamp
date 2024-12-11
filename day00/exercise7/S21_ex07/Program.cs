dda720f104b5736cbd267026cf758c853ae4cca2
zaurbek_home_pc <lordmalsagov@gmail.com>
2024-12-11 19:33:08 +0300

    fix day00 exercise7

--- day00/exercise7/S21_ex07/Program.cs
+++ day00/exercise7/S21_ex07/Program.cs
@@ -14,10 +14,15 @@ namespace S21_ex07
                 Console.WriteLine($"Введите {coupleCount} пар значений в формате (название, цена):");
                 for (int i = 0; i < coupleCount; i++) {
                     var input = Console.ReadLine()?.Split(' ');
-                    var value = input.Any() && !input.Contains("") && double.TryParse(input[1], NumberStyles.Any, CultureInfo.InvariantCulture, out double number) ? number : 0;
-                    if (input.Any() && value > 0)
+                    if (input.Contains("") || !input.Any() || input.Count() < 2)
+                    {
+                        Console.WriteLine("Couldn't parse a words. Please, try again");
+                        continue;
+                    }
+                    var value = double.TryParse(input[1], NumberStyles.Any, CultureInfo.InvariantCulture, out double number) ? number : 0;
+                    if (value > 0 && !result.ContainsKey(input[0]))
                         result.Add(input[0], Math.Round(value, 3));
-                    else Console.WriteLine("Incorrect input. price <= 0");
+                    else Console.WriteLine("Incorrect input. price <= 0 or key already exists");
                 }
                 Console.WriteLine(result.Count>0 ? result?.Select(x => x.Value)?.Average(): "No data");
             }
